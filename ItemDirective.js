50c9395cc4b502a7f2019ab370a0655617efe8b7
laurahk <Laura.Howarth-Kirke@bbc.co.uk>
2014-03-20 12:03:06 +0000

    seperate scopes in directive

--- ItemDirective.js
+++ ItemDirective.js
@@ -1,9 +1,12 @@
+// matches the item tag
 CheckoutModule.directive("item", function() {
     return {
-        restrict: 'E',
-        template: '<button>{{fruit}}</button>',
+        restrict: 'E', // match a tag (element), not an attribute
+        scope: {}, // otherwise gets it's scope from the controller (CheckoutCtrl so you can call scan etc)
+        template: '<button>{{fruit}}</button>', // sticks this html inside the element. Can also set to replace the element if you want.
         link: function(scope, element, attrs) {
-            scope.fruit = attrs.name;
+            scope.fruit = attrs.name; // name of the item element
+            // set this on the scope, so the template function can access it
         }
     };
 });
